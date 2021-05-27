9bc92c088856d98d78d3c46c89b848476fe76cde
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-05-27 03:59:27 +0300

    Add files via upload

---
+++ main/java/Model/RoomFund.java
@@ -0,0 +1,60 @@
+package Model;
+
+public class RoomFund {
+    private int roomId;
+    private double price;
+    private String view;
+    private int capacity;
+    private int roomQuantity;
+
+    public roomFund(){
+    }
+
+    public RoomFund(int roomId, double price, String view, int capacity, int roomQuantity) {
+        this.roomId = roomId;
+        this.price = price;
+        this.view = view;
+        this.capacity = capacity;
+        this.roomQuantity = roomQuantity;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getRoomQuantity() {
+        return roomQuantity;
+    }
+
+    public void setRoomQuantity(int roomQuantity) {
+        this.roomQuantity = roomQuantity;
+    }
+}
