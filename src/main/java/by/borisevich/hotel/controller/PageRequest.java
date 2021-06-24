54051efc4224ba57ca6fcb9d774dc9b7093ad1c5
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-06-24 09:11:43 +0300

    Delete src/main directory

--- src/main/java/by/borisevich/hotel/controller/PageRequest.java
+++
@@ -1,6 +0,0 @@
-package by.borisevich.hotel.controller;
-
-public class PageRequest {
-    public final static String OPEN_PROFILE="";
-    public final static String GO_TO_LOGIN_PAGE="";
-}
--- src/main/java/by/borisevich/hotel/controller/command/CommandName.java
+++
@@ -1,5 +0,0 @@
-package by.borisevich.hotel.controller.command;
-
-public enum CommandName {
-    AUTHORISATION, CREATEBOOKING, REGISTRATION, LOGIN
-}
